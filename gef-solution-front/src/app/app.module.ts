d50cce4eaf94940a5a606820a6eb24e8bafcb31c
Fabricio de Paula Oliveira <fabricio_paula@hotmail.com>
2018-11-24 22:55:31 -0200

    Iniciando o projeto! Menu inicial e componente de inico criados!

---
+++ gef-solution-front/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InicioComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
